cd568f1651a7d6b0efef707c09f4d9e1c4ff6afa
mufeeda1996 <96809025+mufeeda1996@users.noreply.github.com>
2023-11-06 18:14:12 +0400

    first commit

--- src/features/Layout/Layout.jsx
+++ src/features/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Box, Container } from "@mui/material";
 import React from "react";
-import Addressbar from "../../components/AddressBar/Addressbar";
+import Addressbar from "../../components/Addressbar/Addressbar";
 import Navbar from '../../Components/Navbar/Navbar'
 import Footer from "../../Components/Footer/Footer";
 import logo from '../../Image/logo.png'
